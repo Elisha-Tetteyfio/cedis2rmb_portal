134e6f80b96f0033036ced2a9439e54097c6c361
Elisha Tetteyfio <93135004+Elisha-Tetteyfio@users.noreply.github.com>
2024-07-18 17:51:37 +0000

    Add user sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+
+  # Use only devise sessions
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+  }, skip: [:registrations, :passwords, :confirmations, :unlocks, :omniauth_callbacks]
+
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
