8cc5fff64b16854f4c443e06a8886aa235be4247
Elisha Tetteyfio <93135004+Elisha-Tetteyfio@users.noreply.github.com>
2024-09-22 14:07:18 +0000

    add foreman config

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,14 +1,22 @@
 #!/usr/bin/env bash
 # exit on error
+
+set -e
+
+echo "Installing dependencies..."
+bundle install --without development test
+
+echo "Installing foreman..."
+gem install foreman
+
+echo "Building application..."
 set -o errexit
 
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
-
-# If you're using a Free instance type, you need to
-# perform database migrations in the build command.
-# Uncomment the following line:
-
 bundle exec rails db:migrate
 bundle exec rails db:seed
+
+echo "Starting services..."
+foreman start
